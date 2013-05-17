8902bbbc8f5e411b71707baaa9a15b20ed999cb9
toctan <tianjin.sc@gmail.com>
2013-05-17 23:42:56 +0800

    do not display duplicates when searching for history entries

--- lib/history.zsh
+++ lib/history.zsh
@@ -9,6 +9,7 @@ setopt append_history
 setopt extended_history
 setopt hist_expire_dups_first
 setopt hist_ignore_dups # ignore duplication command history list
+setopt hist_find_no_dups # do not display duplicates when serarching for history entries
 setopt hist_ignore_space
 setopt hist_verify
 setopt inc_append_history
