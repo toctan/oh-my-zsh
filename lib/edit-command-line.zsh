4f1bd2889056dffb49f9dd45e1514fe99dff12c0
toctan <tianjin.sc@gmail.com>
2013-10-07 16:37:32 +0800

    Use Ctrl-x, Ctrl-i insert output of last command

--- lib/edit-command-line.zsh
+++ lib/edit-command-line.zsh
@@ -1,3 +1,10 @@
 autoload -U edit-command-line
 zle -N edit-command-line
 bindkey '\C-x\C-e' edit-command-line
+
+zmodload -i zsh/parameter
+insert-last-command-output() {
+LBUFFER+="$(eval $history[$((HISTCMD-1))])"
+}
+zle -N insert-last-command-output
+bindkey '\C-x\C-i' insert-last-command-output
